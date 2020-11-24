9a9edf6f14d7c75f3b0265a07a2c2214872c6a80
Tarik <tariksnow37@gmail.com>
2020-11-23 18:28:52 -0800

    Update(:Clean coupling, and garbage collection)

--- src/components/Math/MathDefaultBox.tsx
+++ src/components/Math/MathDefaultBox.tsx
@@ -1,11 +1,20 @@
+import { render } from '@testing-library/react';
 import React, { useEffect, useRef } from 'react';
 import * as THREE from 'three';
+import { disposeHierarchy, disposeNode } from '../Util/garbageCollectNode';
 
 const MathDefaultBox = (props: { active: boolean }) => {
     let id: any;
 
-    const ctxRef = useRef<HTMLHeadingElement>(null);
     const reqRef = useRef<any>();
+    const ctxRef = useRef<HTMLHeadingElement | null>(null);
+    const sceRef = useRef<THREE.Scene>(new THREE.Scene());
+    const camRef = useRef<THREE.PerspectiveCamera>(
+        new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    );
+    const renRef = useRef<THREE.WebGLRenderer>(
+        new THREE.WebGLRenderer({ antialias: true })
+    );
 
     useEffect(() => {
         if (props.active) {
@@ -16,9 +25,9 @@ const MathDefaultBox = (props: { active: boolean }) => {
     }, [props.active]);
 
     const initViewPort = () => {
-        let scene = new THREE.Scene();
-        let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-        let renderer = new THREE.WebGLRenderer({ antialias: true });
+        let scene = sceRef.current;
+        let camera = camRef.current;
+        let renderer = renRef.current;
 
         renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -38,7 +47,11 @@ const MathDefaultBox = (props: { active: boolean }) => {
         scene.add(cube);
         camera.position.z = 5;
 
-        let animate = () => {
+        sceRef.current = scene;
+        camRef.current = camera;
+        renRef.current = renderer;
+
+        let animate = () : void => {
             reqRef.current = requestAnimationFrame(animate);
             cube.rotation.x += 0.01;
             cube.rotation.y += 0.01;
@@ -49,6 +62,9 @@ const MathDefaultBox = (props: { active: boolean }) => {
 
     const cancelVis = () => {
         cancelAnimationFrame(reqRef.current);
+        disposeHierarchy(sceRef.current, disposeNode);
+        let canvas = document.getElementsByTagName('CANVAS')
+        canvas[0].remove();
     }
 
     return (
